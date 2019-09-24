520bf636f97211971613ce7d17ba8fbe5b67be45
OAbin <18109402716@163.com>
2019-09-25 01:42:12 +0800

    create simple maven web application

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mavenwebapplication
 简单的maven结构的web application
+
+比较简单的maven web application   
+maven + jsp + servlet  
+需要手动创建webapp文件夹
+
---
+++ src/main/java/com/binge/mavenweb/HelloMavenWebServlet.java
@@ -0,0 +1,20 @@
+package com.binge.mavenweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloMavenWebServlet" ,urlPatterns = "/maven/hello")
+public class HelloMavenWebServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("welcome to maven web application!!!");
+    }
+}
